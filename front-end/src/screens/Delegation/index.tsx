65b5363581c3e0e453d0338ae2d9649e089ed7a9
Arvianto Dwi <arvi@ata.network>
2021-12-08 16:54:49 +0700

    WIP: delegation management page

--- front-end/src/screens/Delegation/index.tsx
+++ front-end/src/screens/Delegation/index.tsx
@@ -2,8 +2,86 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import styled from '@xstyled/styled-components'
 import React from 'react'
+import { Divider, Grid } from 'semantic-ui-react'
 
-const Delegation = () => <></>
+import Address from './address'
 
-export default Delegation
+interface Props {
+  className?: string
+}
+
+const Delegation = ({ className }: Props) => {
+  return (
+    <Grid>
+      <Grid.Column
+        className={className}
+        mobile={16}
+        tablet={12}
+        computer={12}
+        largeScreen={10}
+        widescreen={10}
+      >
+        <h2>Manage Delegation</h2>
+        <Divider />
+        <Address />
+      </Grid.Column>
+      <Grid.Column only="computer" computer={4} largeScreen={6} widescreen={6} />
+    </Grid>
+  )
+}
+
+export default styled(Delegation)`
+  background-color: white;
+  padding: 2rem 3rem 3rem 3rem !important;
+  border-radius: 0.3rem;
+  box-shadow: ${({ theme }) => theme.colors.box_shadow_card};
+
+  .ui.divider,
+  .ui.divider:not(.vertical):not(.horizontal) {
+    margin: 3rem 0 2rem 0;
+    border-top-style: solid;
+    border-top-width: 1px;
+    border-top-color: ${({ theme }) => theme.colors.grey_light};
+    border-bottom: none;
+  }
+
+  .button {
+    margin-top: 0.2rem;
+  }
+
+  .ui.form:not(.unstackable) .fields:not(.unstackable) > .ten.wide.field {
+    @media only screen and (max-width: 767px) {
+      width: 70% !important;
+    }
+
+    @media only screen and (max-width: 576px) {
+      width: 60% !important;
+    }
+  }
+
+  .ui.form:not(.unstackable) .fields:not(.unstackable) > .six.wide.field {
+    @media only screen and (max-width: 767px) {
+      width: 30% !important;
+    }
+
+    @media only screen and (max-width: 576px) {
+      width: 40% !important;
+    }
+  }
+
+  @media only screen and (max-width: 576px) {
+    padding: 2rem !important;
+
+    .ui.form {
+      margin-top: 0;
+      padding: 0;
+    }
+
+    button {
+      padding: 0.8rem 1rem;
+      border-radius: 0.5rem;
+    }
+  }
+`
