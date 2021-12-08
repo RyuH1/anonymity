ea36cf9ddc81907ee860d81458e46e5d41e6eb56
Arvianto Dwi <arvi@ata.network>
2021-12-08 16:54:19 +0700

    Add delegatees global constant

---
+++ front-end/src/global/delegatees.ts
@@ -0,0 +1,16 @@
+// Copyright 2019-2020 @Premiurly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { Delegatee } from '../types'
+
+export const DELEGATEES: Delegatee[] = [
+  {
+    name: 'Geode Endpoint (EVE)',
+    url: '5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw'
+  },
+  {
+    name: 'Geode Endpoint (FERDIE)',
+    url: '5CiPPseXPECbkjWCa6MnjNokrgYjMqmKndv2rSnekmSK2DjL'
+  }
+]
