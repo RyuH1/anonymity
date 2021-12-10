ab8ded73389c25e8d7cd5c7c9e44c1e9ebafdcd7
RyuH1 <haichuan@ata.network>
2021-12-10 21:20:36 +0800

    Fix decimal

--- front-end/src/global/networkConstants.ts
+++ front-end/src/global/networkConstants.ts
@@ -24,7 +24,7 @@ export const chainProperties: ChainPropType = {
   [network.POLKADOT]: {
     blockTime: 6000,
     ss58Format: 0,
-    tokenDecimals: 10,
+    tokenDecimals: 18,
     tokenSymbol: tokenSymbol.DOT
   }
 }
